113beea3359af296c7816590b92ea6dd4f71d878
TiagoFar78 <tiagoagostinhofarinha@gmail.com>
2025-07-21 23:54:36 +0100

    add: game management

---
+++ src/main/java/net/tiagofar78/pirates/game/PGame.java
@@ -0,0 +1,16 @@
+package net.tiagofar78.pirates.game;
+
+
+public class PGame {
+    
+    private int _id;
+    
+    public PGame(int id) {
+        _id = id;
+    }
+    
+    public int getId() {
+        return _id;
+    }
+
+}
---
+++ src/main/java/net/tiagofar78/pirates/managers/GameManager.java
@@ -0,0 +1,45 @@
+package net.tiagofar78.pirates.managers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.tiagofar78.pirates.game.PGame;
+
+public class GameManager {
+
+    private static final int MAX_GAMES = 0;
+
+    private static PGame[] games = new PGame[MAX_GAMES];
+
+    public static PGame getGame(int id) {
+        for (PGame game : games) {
+            if (game != null && game.getId() == id) {
+                return game;
+            }
+        }
+
+        return null;
+    }
+
+    public static List<Integer> getGamesIds() {
+        List<Integer> ids = new ArrayList<>();
+
+        for (PGame game : games) {
+            if (game != null) {
+                ids.add(game.getId());
+            }
+        }
+
+        return ids;
+    }
+
+    public static void removeGame(int id) {
+        for (int i = 0; i < games.length; i++) {
+            if (games[i] != null && games[i].getId() == id) {
+                games[i] = null;
+            }
+        }
+    }
+
+}
+
